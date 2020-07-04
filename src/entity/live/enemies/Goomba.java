3832661daf1d7999232067600bd6d9cc6c7587d2
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2020-07-04 10:29:06 +0200

    Fix: Make easy mode playable
    The goomba image needs to be edited, but for now it's fine

--- src/entity/live/enemies/Goomba.java
+++ src/entity/live/enemies/Goomba.java
@@ -7,7 +7,7 @@ public class Goomba extends Enemy {
     public Goomba() {
         this.life = 30;
         this.name = "Goomba";
-        this.imagePath = "Goomba.jpg";
+        this.imagePath = "Goomba.png";
         this.quantityDamage = 4;
     }
 
