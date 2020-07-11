29000399c468617703d90d291fa19fd6d61d6c8b
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2020-07-11 10:56:08 +0200

    Heroes: add Peach Princess
    Maybe it would be better to add a new factory (medium level factory)
    that faces Peach and Koopa Troopa

---
+++ src/entity/live/heroes/Peach.java
@@ -0,0 +1,12 @@
+package entity.live.heroes;
+
+public class Peach extends Hero {
+
+    public Peach() {
+        this.life = 80;
+        this.name = "Princess Peach";
+        this.imagePath = "Peach.jpg";
+        this.damage = 18;
+    }
+
+}
--- src/factory/types/DifficultEntity.java
+++ src/factory/types/DifficultEntity.java
@@ -5,6 +5,7 @@ import entity.live.enemies.Enemy;
 import entity.live.enemies.KoopaTroopa;
 import entity.live.heroes.Hero;
 import entity.live.heroes.Mario;
+import entity.live.heroes.Peach;
 import factory.EntityFactory;
 
 
@@ -12,8 +13,9 @@ public class DifficultEntity extends EntityFactory {
 
     @Override
     public Hero getHero() {
+        randomNumber = rand.nextInt(20);
         // NOTE: this needs to change if you want to add more heroes
-        return new Mario();
+        return randomNumber < 10 ? new Mario() : new Peach();
     }
 
     @Override
